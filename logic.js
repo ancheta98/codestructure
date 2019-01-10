2c22d74b95d148b3d870e8ab71896f8c24499797
Angel Ancheta <angelancheta98@yahoo.com>
2019-01-09 17:54:45 -0800

    helping out

---
+++ logic.js
@@ -0,0 +1,33 @@
+// Fix structure of code
+
+//all the requires at the top
+
+//Create all the functions
+
+function spotify(){
+    //code here
+}
+
+function concert(){
+
+}
+
+function movie(){
+
+}
+
+function doWhatItSays(){
+    //from the data array 
+    //if data[0] == spotify then run spotify()
+
+}
+
+//Have if statements that check the user input to know what function to run
+
+if(process.argv[2] == "spotify-this"){
+    //do something
+    spotify(process.argv[3])
+}
+else if(process.argv[2] == "movie-this"){
+    //continue
+}
